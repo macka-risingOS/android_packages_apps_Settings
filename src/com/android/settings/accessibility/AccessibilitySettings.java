46582a6388240a9d1215a8a74dfd5d164c38069c
Yuan Xiangjing <yuan.xiangjing@zte.com.cn>
2023-12-18 17:20:13 +0800

    Fix issue on icons refreshed twice by onCreate and onResume
    Test:
    
    Entering the Settings Accessibility page, then check the icons
    of downloaded apps (such as TalkBack).
    
    Change-Id: I10846c9549cbb5147ba5ba8539111965c0291ee2
    Signed-off-by: Jia Jia <jia.jia@zte.com.cn>

--- src/com/android/settings/accessibility/AccessibilitySettings.java
+++ src/com/android/settings/accessibility/AccessibilitySettings.java
@@ -157,6 +157,8 @@ public class AccessibilitySettings extends DashboardFragment {
     private boolean mNeedPreferencesUpdate = false;
     private boolean mIsForeground = true;
 
+    private boolean mUpdatedInOnCreate = false;
+
     public AccessibilitySettings() {
         // Observe changes to anything that the shortcut can toggle, so we can reflect updates
         final Collection<AccessibilityShortcutController.FrameworkFeatureInfo> features =
@@ -200,12 +202,19 @@ public class AccessibilitySettings extends DashboardFragment {
         initializeAllPreferences();
         updateAllPreferences();
         registerContentMonitors();
+
+        mUpdatedInOnCreate = true;
     }
 
     @Override
     public void onResume() {
         super.onResume();
-        updateAllPreferences();
+
+        if (mUpdatedInOnCreate) {
+            mUpdatedInOnCreate = false;
+        } else {
+            updateAllPreferences();
+        }
     }
 
     @Override
